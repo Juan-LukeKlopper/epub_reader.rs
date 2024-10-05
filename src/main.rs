a4ad4d6ddcdd50efb4ffd422d3b8fd48699397cf
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-05 12:48:28 +0200

    chore: remove dead code from implementing ratatui

--- src/main.rs
+++ src/main.rs
@@ -128,20 +128,3 @@ fn main() -> io::Result<()> {
     ratatui::restore();
     app_result
 }
-
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                .white()
-                .on_blue();
-            frame.render_widget(greeting, frame.area());
-        })?;
-
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
-            }
-        }
-    }
-}
